7e45dfd9341dab358b632ab0d1d95e02c157eab8
darrenkjr <45780943+darrenkjr@users.noreply.github.com>
2022-06-20 23:25:12 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,5 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"darren.rajit1@monash.edu\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "[server]"  > ~/.streamlit/config.toml 
+echo "headless = true"  >> ~/.streamlit/config.toml
+echo "port = $PORT"  >> ~/.streamlit/config.toml
+echo "enableCORS = false"  >> ~/.streamlit/config.toml
