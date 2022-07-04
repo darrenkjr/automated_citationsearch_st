1b4fb44ef6c2404f1cd324d3f1e2258932793c75
darrenkjr <darren.rajit@gmail.com>
2022-07-04 14:05:55 +1000

    still tyring to fix setup sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
+mkdir -p ~/.streamlit/
+echo "[general]"  > ~/.streamlit/credentials.toml
+echo "email = \"my@email.com\""  >> ~/.streamlit/credentials.toml
 echo "[server]"  > ~/.streamlit/config.toml 
 echo "headless = true"  >> ~/.streamlit/config.toml
 echo "port = $PORT"  >> ~/.streamlit/config.toml
-echo "enableCORS = false"  >> ~/.streamlit/config.toml
+echo "enableCORS = true"  >> ~/.streamlit/config.toml
